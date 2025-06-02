cc16ae7a80d27a84e5bcf7007e6ac422d9c2ddab
Erlend Johnsen <erea@ihelse.net>
2025-06-02 09:22:29 +0200

    adds logo

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -12,20 +12,23 @@ export default function Header() {
   return (
     <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky border-b p-4 backdrop-blur">
       <div className="flex items-center justify-between">
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuLink asChild>
-                <NavLink to="/">Home</NavLink>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink asChild>
-                <NavLink to="/contact">Contact</NavLink>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <div className='flex items-center gap-4'>
+          <img src="assets/ecom.svg" className="bg-white rounded-full p-1" width="60" alt="ecom store logo" />
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <NavLink to="/">Home</NavLink>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <NavLink to="/contact">Contact</NavLink>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
         <div className="flex gap-2">
           <ThemeToggle />
           <CartIcon />
