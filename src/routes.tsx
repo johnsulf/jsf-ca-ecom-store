c56125a9ed0802f1a21784b27a811bddfc9ffad6
johnsulf <erlendjohns@gmail.com>
2025-06-08 12:43:42 +0200

    adds jsdocs for routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,3 +1,9 @@
+/**
+ * Application routing module.
+ *
+ * @module AppRoutes
+ * @description Defines the application's route configuration using React Router.
+ */
 import { Route, Routes } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Cart from './pages/Cart/Cart';
@@ -6,6 +12,12 @@ import Home from './pages/Home/Home';
 import Product from './pages/Product/Product';
 import Checkout from './pages/Checkout/Checkout';
 
+/**
+ * AppRoutes component that renders the application's routes.
+ *
+ * @component
+ * @returns The routing configuration for the application.
+ */
 export default function AppRoutes() {
   return (
     <Routes>
