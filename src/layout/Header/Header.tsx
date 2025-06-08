ab5a4e31d8d34886b2b1fd6f69bce6a774e88bc8
johnsulf <erlendjohns@gmail.com>
2025-06-08 11:43:27 +0200

    adds max width to main- and header content

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -11,30 +11,30 @@ import CartIcon from './components/CartIcon';
 export default function Header() {
   return (
     <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky border-b p-4 backdrop-blur">
-      <div className="flex items-center justify-between">
-        <div className='flex items-center gap-4'>
-          <a href="/">
-            <img src="assets/ecom.svg" className="bg-white rounded-full p-1" width="60" alt="ecom store logo" />
-          </a>
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <NavLink to="/">Home</NavLink>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <NavLink to="/contact">Contact</NavLink>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
-        <div className="flex gap-2">
-          <ThemeToggle />
-          <CartIcon />
-        </div>
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+      <div className='flex items-center gap-4'>
+        <a href="/">
+        <img src="assets/ecom.svg" className="bg-white rounded-full p-1" width="60" alt="ecom store logo" />
+        </a>
+        <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <NavLink to="/">Home</NavLink>
+          </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <NavLink to="/contact">Contact</NavLink>
+          </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <div className="flex gap-2">
+        <ThemeToggle />
+        <CartIcon />
+      </div>
       </div>
     </header>
   );
