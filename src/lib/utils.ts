b789c9e798bd0c036d620efeb087d0cb58ca37e6
johnsulf <erlendjohns@gmail.com>
2025-06-08 13:01:47 +0200

    adds comments and jsdocs to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
+/**
+ * Utility functions module.
+ *
+ * @module Utils
+ * @description Provides helper functions for class name concatenation and merging.
+ */
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Combines and merges Tailwind CSS class names.
+ *
+ * Applies `clsx` to conditionally join class values and `twMerge` to dedupe and merge them.
+ *
+ * @param inputs - Class values to process.
+ * @returns The final class string.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
